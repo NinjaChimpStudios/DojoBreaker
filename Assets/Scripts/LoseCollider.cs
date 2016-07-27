ca6fbc87d0582cc1ab80040a714c5b9efc3e7e98
James Macbox1 <james@radsy.com>
2016-07-27 21:59:56 +1000

    initial build

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	private LevelManager levelManager;
+	
+	void OnTriggerEnter2D (Collider2D trigger) {
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		levelManager.LoadLevel("LoseScreen");
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		print ("Collision");	
+	}
+	
+}
