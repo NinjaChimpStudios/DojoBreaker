d64876004d84afa28998e9fc8646f75236ae8f65
James Macbox1 <james@radsy.com>
2016-08-11 23:17:18 +1000

    analytics for DOJO-18 and tweaks to scoring

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
 
 public class GameManager : MonoBehaviour {
 
@@ -13,6 +14,10 @@ public class GameManager : MonoBehaviour {
 	private const int defaultStackSize = 100;
 	private Text scoreText;
 	private const string levelPrefix = "Level_";
+	private string highestLevel = "Splash";
+	private string latestLevel = "Splash";
+	private int highestScore = 0;
+	private int gameCount = 0;
 	
 	void Awake() {
 		if (Instance != null && Instance != this) {
@@ -46,10 +51,13 @@ public class GameManager : MonoBehaviour {
 			c.collisionType,
 			collisions.Count ));
 		int scoreMult = 1;
+		bool doubleBat = false;
 		if (collisions.Count > 0) {
 			Collision prev = collisions.Peek();
 			if (prev.collisionType != Collision.CollisionType.Bat) {
 				scoreMult = 2; // double score if 2 brick hits in a row
+			} else if (c.collisionType == Collision.CollisionType.Bat) {
+				doubleBat = true; // 2 bat hits in a row = bad
 			}
 		}
 		collisions.Push(c);
@@ -59,6 +67,9 @@ public class GameManager : MonoBehaviour {
 				break;
 			case Collision.CollisionType.Break :
 				theScore += (2 * scoreMult);
+				break;
+			case Collision.CollisionType.Bat :
+				theScore += (doubleBat) ? -1 : 0;
 				break;
 			default :
 				break;
@@ -73,5 +84,34 @@ public class GameManager : MonoBehaviour {
 	public void ResetCollisionCount() {
 		collisions.Clear();
 	}
+
+	public void ReportGameStart() {
+
+	}
+
+	public void ReportLevelStart() {
+
+	}
+	
+	public void ReportLevelEnd() {
+		
+	}
+	
+	public void ReportGameEnd() {
+
+	}
+	
+	public void ReportQuit() {
+		Dictionary<string, object> d = new Dictionary<string, object> {
+			{ "highestLevel", highestLevel },
+    		{ "highestScore", highestScore },
+    		{ "gameCount", gameCount }
+		};
+		ReportAnalytics("dojobreaker.quit", d);
+	}
+
+	private void ReportAnalytics(string tag, Dictionary<string, object> data) {
+		// Analytics.CustomEvent(tag, data);
+  	}
 
 }
